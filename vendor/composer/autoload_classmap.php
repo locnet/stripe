b025b332373eac4052f1ef4afb8b04f24a54b5b0
locnet <locnetarganda@gmail.com>
2017-10-05 20:42:55 +0200

    stripe fix

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -25,6 +25,7 @@ return array(
     'App\\Http\\Middleware\\TrustProxies' => $baseDir . '/app/Http/Middleware/TrustProxies.php',
     'App\\Http\\Middleware\\VerifyCsrfToken' => $baseDir . '/app/Http/Middleware/VerifyCsrfToken.php',
     'App\\Links' => $baseDir . '/app/Links.php',
+    'App\\Payments' => $baseDir . '/app/Payments.php',
     'App\\Providers\\AppServiceProvider' => $baseDir . '/app/Providers/AppServiceProvider.php',
     'App\\Providers\\AuthServiceProvider' => $baseDir . '/app/Providers/AuthServiceProvider.php',
     'App\\Providers\\BroadcastServiceProvider' => $baseDir . '/app/Providers/BroadcastServiceProvider.php',
